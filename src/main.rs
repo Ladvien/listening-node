df31546d734c86aa3bea9367e04e838de1d38e83
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2025-01-28 06:22:02 -0600

    Rust

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ pub const POLLING_SECONDS: f32 = 5f32;
 
 fn main() {
     let model_definition = whisper::monolingual::Definition::new(
-        whisper::monolingual::ModelType::DistilLargeEnV3,
+        whisper::monolingual::ModelType::MediumEn,
         norma::models::SelectedDevice::Metal,
     );
 
@@ -15,26 +15,27 @@ fn main() {
     // 1. Poll the audio stream
     // 2. If there is silence, then stop the stream and transcribe
     // 3. If there is no silence, then continue polling
-    loop {
-        let (join_handle, transcriber_handle) =
-            Transcriber::blocking_spawn(model_definition.clone()).unwrap();
+    // loop {
+    let (join_handle, transcriber_handle) =
+        Transcriber::blocking_spawn(model_definition.clone()).unwrap();
 
-        let mut stream = transcriber_handle
-            .blocking_start(norma::input::Settings::default())
-            .unwrap();
+    let mut stream = transcriber_handle
+        .blocking_start(norma::input::Settings::default())
+        .unwrap();
 
-        thread::spawn(move || {
-            while let Some(seg) = stream.blocking_recv() {
-                println!("{}", seg);
-            }
-        });
+    thread::spawn(move || {
+        while let Some(seg) = stream.blocking_recv() {
+            println!("{}", seg);
+        }
+    });
 
-        thread::sleep(Duration::from_secs_f32(POLLING_SECONDS));
+    thread::sleep(Duration::from_secs_f32(POLLING_SECONDS));
 
-        // transcriber_handle.stop().unwrap();
-        // drop(transcriber_handle);
+    transcriber_handle.stop().unwrap();
+    drop(transcriber_handle);
 
-        join_handle.join().unwrap().unwrap();
-    }
+    join_handle.join().unwrap().unwrap();
+
     println!("Restarting...");
+    // }
 }
